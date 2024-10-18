e80323fddb552841304dba7c954a19a04a989de8
nrismawardah <nrismawardah@gmail.com>
2024-10-18 22:05:54 +0700

    Commit 11: Menambah elemen image dengan id resource R.drawable.umy dan dengan top-padding 20dp dan size 200dp

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basiccompose.ui.theme.BasicComposeTheme
 
@@ -48,6 +52,9 @@ fun BasicLayout(modifier: Modifier = Modifier) {
             fontWeight = FontWeight.Bold)
         )
         Text(text = "Ini adalah halaman login"
+        )
+        Image(painter = painterResource(id = R.drawable.umy), contentDescription = null,
+            modifier = Modifier.padding(top = 20.dp).size(200.dp)
         )
     }
 }
