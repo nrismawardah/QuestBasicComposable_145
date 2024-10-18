50cb6e9bd4d83d88bb1bf2ec6ddfd8e652006279
nrismawardah <nrismawardah@gmail.com>
2024-10-18 21:45:51 +0700

    Commit 4: Menambahkan layout composable Column untuk menyusun elemen secara vertikal

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -33,10 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicLayout(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $!",
-        modifier = modifier
-    )
+    Column(
+
+    ){
+
+    }
 }
 
 @Preview(showBackground = true)
