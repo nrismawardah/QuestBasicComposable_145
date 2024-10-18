59d3d736fb54dea1964986cdecb6338c401f30e7
nrismawardah <nrismawardah@gmail.com>
2024-10-18 21:48:21 +0700

    Commit 5: Menambahkan parameter horizontalAlignment untuk mensejajarkan elemen di Column ke tengah secara horizontal

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.basiccompose.ui.theme.BasicComposeTheme
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicLayout(modifier: Modifier = Modifier) {
     Column(
-
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
 
     }
